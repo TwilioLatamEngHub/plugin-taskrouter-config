dc77eb0e0d03fd210f5524a6617d106969ee0a92
Gus Varallo <gustavo@varallo.me>
2022-08-23 19:00:25 -0300

    feat: add fetch-workspace function

---
+++ serverless-taskrouter-config/functions/fetch-workspace.js
@@ -0,0 +1,28 @@
+const path = Runtime.getFunctions()['response-header'].path
+const response = require(path).response()
+
+exports.handler = async function (context, event, callback) {
+  const client = context.getTwilioClient()
+  const workspaceSid = context.WORKSPACE_SID
+
+  await client.taskrouter.v1
+    .workspaces(workspaceSid)
+    .fetch()
+    .then(
+      async workspace =>
+        await client.taskrouter.v1
+          .workspaces(workspaceSid)
+          .activities.list()
+          .then(activities =>
+            response.setBody({
+              workspaceName: workspace.friendlyName,
+              activities
+            })
+          )
+    )
+    .then(() => callback(null, response))
+    .catch(error => {
+      console.log(error)
+      callback(error)
+    })
+}
